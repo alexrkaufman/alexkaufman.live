a7f3c3399ce99a474676ddb55b603c654726c945
Alex Kaufman <arkaufman@protonmail.com>
2025-01-17 07:50:30 -0700

    making script for automatic updates

--- update-site.sh
+++ update-site.sh
@@ -1,4 +1,15 @@
-pushd "./md-webapp"
+#! /usr/bin/bash
+
+source "/home/dustiestgolf/.virtualenvs/base/bin/activate"
+
+pushd "/home/dustiestgolf/md-webapp"
+pushd "/home/dustiestgolf/md-webapp/alexkaufmanlive/content"
+
 git fetch
 git reset --hard origin/main
+popd
+
+git fetch
+git reset --hard origin/main
+flask --app alexkaufmanlive update-db
 popd
