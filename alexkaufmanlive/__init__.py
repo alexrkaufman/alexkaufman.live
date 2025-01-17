568246cb3431ea80f5114d1479c2ca910ca8d74c
Alex Kaufman <arkaufman@protonmail.com>
2025-01-17 08:15:55 -0700

    moving images into content

--- alexkaufmanlive/__init__.py
+++ alexkaufmanlive/__init__.py
@@ -16,7 +16,7 @@ site_metadata = {
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="content/static")
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "alexkaufmanlive.sqlite"),
