3d2bce0e87aa44bd7c8f347bafb544a30a455b61
Alex Kaufman <arkaufman@protonmail.com>
2025-11-22 08:09:11 -0700

    deleting now unnecessary files

--- alexkaufmanlive_wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-path = "/home/dustiestgolf/alexkaufmanlive"
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-from alexkaufmanlive import create_app
-
-application = create_app()
