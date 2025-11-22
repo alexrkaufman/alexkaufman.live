ed2c55cebf6c737917782f6dbe1d8a4d8a7fbe84
Alex Kaufman <arkaufman@protonmail.com>
2025-11-22 04:54:13 -0700

    move main page logic into its own file
    - create routes folder
      - set up module with `route/__init__.py`
      - create `routes/main.py`
    - remove main page rendering logic from `__init__.py`
    - register main as a blueprint in the app in `__init__.py`

--- alexkaufmanlive/__init__.py
+++ alexkaufmanlive/__init__.py
@@ -1,21 +1,9 @@
 import os
-import pathlib
 
-import frontmatter
 from flask import (
     Flask,
-    current_app,
-    get_template_attribute,
-    jsonify,
-    make_response,
     render_template,
-    request,
 )
-from flask.helpers import redirect
-
-from .db import get_db
-from .services.email import subscribe_to_buttondown
-from .services.markdown import render_page
 
 site_metadata = {
     "site_name": "alexkaufman.live",
@@ -48,92 +36,6 @@ def create_app(test_config=None):
     def inject_sitename():
         return site_metadata
 
-    @app.route("/")
-    def home_page():
-        """Builds the home page of the site."""
-
-        db = get_db()
-        home_path = pathlib.Path(current_app.root_path) / "content/home.md"
-
-        upcoming_shows = db.execute(
-            (
-                "SELECT id, title, show_date, link, meta"
-                " FROM shows"
-                " WHERE show_date >= CURRENT_DATE"
-                " ORDER BY show_date ASC"
-            )
-        ).fetchall()
-
-        home = frontmatter.load(str(home_path))
-        content = render_page(
-            home.content,
-            upcoming_shows=upcoming_shows,
-            show_list=get_template_attribute("parts.jinja2", "show_list"),
-            email_list_cta=get_template_attribute("parts.jinja2", "email_list_cta"),
-        )
-
-        return render_template(
-            "base.jinja2", content=content, title="alexkaufman.live", page_class="home"
-        )
-
-    @app.route("/sitemap")
-    @app.route("/sitemap/")
-    @app.route("/sitemap.xml")
-    def sitemap():
-        """
-        Route to dynamically generate a sitemap of your website/application.
-        lastmod and priority tags omitted on static pages.
-        lastmod included on dynamic content such as blog posts.
-        """
-        from urllib.parse import urlparse
-
-        host_components = urlparse(request.host_url)
-        host_base = host_components.scheme + "://" + host_components.netloc
-
-        urls = list()
-        # Static routes with static content
-        for rule in app.url_map.iter_rules():
-            if "GET" in rule.methods and len(rule.arguments) == 0:
-                url = {"loc": f"{host_base}{str(rule)}"}
-                urls.append(url)
-
-        # Dynamic routes with dynamic content
-        db = get_db()
-        shows = db.execute(
-            "SELECT id, title, show_date, link FROM shows ORDER BY show_date ASC"
-        ).fetchall()
-        for show in shows:
-            url = {
-                "loc": f"{host_base}/shows/{show['link']}",
-            }
-            urls.append(url)
-
-        xml_sitemap = render_template(
-            "sitemap_template.jinja2",
-            urls=urls,
-            host_base=host_base,
-        )
-        response = make_response(xml_sitemap)
-        response.headers["Content-Type"] = "application/xml"
-        return response
-
-    @app.route("/blog/")
-    def blog_redirect():
-        return redirect("https://blog.alexkaufman.live", code=302)
-
-    @app.route("/api/subscribe", methods=["POST"])
-    def email_subscribe():
-        """Handle email subscription via Buttondown API."""
-        email = request.form.get("email")
-        tags = request.form.getlist("tag")
-
-        success, message, status_code = subscribe_to_buttondown(email, tags)
-
-        if success:
-            return jsonify({"success": True, "message": message}), status_code
-        else:
-            return jsonify({"success": False, "error": message}), status_code
-
     @app.errorhandler(404)
     def page_not_found(error):
         content = {
@@ -152,7 +54,9 @@ def create_app(test_config=None):
         return render_template("error.jinja2", **content), 500
 
     from . import shows
+    from .routes import main
 
+    app.register_blueprint(main.bp)
     app.register_blueprint(shows.bp)
 
     from . import db
