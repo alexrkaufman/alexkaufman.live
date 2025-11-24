f2011e7f6e5c239d1833506acf23967cd8ace3c6
Alex Kaufman <arkaufman@protonmail.com>
2025-11-24 00:03:13 -0700

    making update-site update packages if pyproject.toml has been updated

--- update-site.sh
+++ update-site.sh
@@ -4,8 +4,18 @@ source "/home/dustiestgolf/venv/bin/activate"
 
 pushd "/home/dustiestgolf/alexkaufman.live"
 
+# Store the current commit hash before updating
+OLD_COMMIT=$(git rev-parse HEAD)
+
 git fetch
 git reset --hard origin/main
+
+# Check if pyproject.toml was modified in the update
+if git diff --name-only $OLD_COMMIT HEAD | grep -q "pyproject.toml"; then
+    echo "pyproject.toml was updated, reinstalling dependencies..."
+    pip install -e .
+fi
+
 flask --app alexkaufmanlive update-db
 
 # Touch WSGI file to reload the web app
